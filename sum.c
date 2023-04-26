aacc369565eb4f4384c709a31b90981e30eb2da9
imolina97 <128269336+imolina97@users.noreply.github.com>
2023-04-25 20:30:27 -0700

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// This program sums two integers that are hard coded in the main program
+
+int main()
+{
+  // initialize program
+  int num1 = 5, num2 = 6;
+  
+  // print sum of numbers
+  printf("The sum is: %d\n", num1 + num2);
+  
+  // end program
+  return 0;
+}
